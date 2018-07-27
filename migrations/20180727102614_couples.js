c4c15e5f60bc93643d96d86fb57d05f304f2e174
rstatz <robinstatz@gmail.com>
2018-07-27 14:19:50 -0600

    migrated latest.

--- migrations/20180727102614_couples.js
+++ migrations/20180727102614_couples.js
@@ -2,10 +2,10 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('couples', table => {
     table.increments()
-    table.integer('user1_id').notNullable().defaultsTo(0)
+    table.integer('user1_id').notNullable()
     table.foreign('user1_id').references('users.id').onDelete('CASCADE')
 
-    table.integer('user2_id').notNullable().defaultsTo(0)
+    table.integer('user2_id').notNullable()
     table.foreign('user2_id').references('users.id').onDelete('CASCADE')
 
     table.unique(['user1_id', 'user2_id'])
--- migrations/20180727102722_users_feelings.js
+++ migrations/20180727102722_users_feelings.js
@@ -1,14 +1,16 @@
 
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('user_feelings', table => {
+  return knex.schema.createTable('users_feelings', table => {
     table.increments()
-    table.integer('user_id').notNullable().defaultsTo()
+    table.integer('user_id').notNullable()
     table.foreign('user_id').references('users.id').onDelete('CASCADE')
 
-    table.integer('feeling_id').notNullable().defaultsTo('')
+    table.integer('feeling_id').notNullable()
     table.foreign('feeling_id').references('feelings.id').onDelete('CASCADE')
-};
+  })
+
+}
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('user_feelings')
+  return knex.schema.dropTable('users_feelings')
 };
