3cae35340443e4b70c578013a22dc72628b9ebed
Melissa Pizarro <mpizarro03@gmail.com>
2018-07-28 16:42:25 -0600

    fixed file path names

--- routes/couples.js
+++ routes/couples.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const ctrl = require('../controller/couples')
 
+
 router.post('/', ctrl.createCouple)
 router.delete('/:id', ctrl.deleteCouple)
 
